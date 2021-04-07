6c30e3ec76d4f5e9e74340838d45b06176b221a9
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-04-07 17:28:06 +0200

    Add solutions

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,9 @@ const product = {
   screenType: 'LED',
 }
 
+const { price, refreshRate } = product;
+console.log(price, refreshRate);
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +20,9 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const { type, brand } = productInformation.general;
+console.log(type, brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,15 +44,22 @@ const tvOptions = {
   },
 };
 
+const { wifi, bluetooth } = tvOptions.options.connectivity.popular;
+console.log(wifi, bluetooth);
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
 
+const [wifiSignal, bluetoothSignal] = connectivity;
+console.log(wifiSignal, bluetoothSignal);
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
 
+const [first,, third] = tvSizes;
+console.log(first, third);
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
@@ -57,8 +70,14 @@ function getCompanyDetails() {
   }
 }
 
+const { name, adress } = getCompanyDetails();
+console.log(name, adress);
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const [nameString, ageString] = getDetails('Nova', 27);
+console.log(nameString, ageString);
