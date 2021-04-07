1d55dfd611c5b7a20749c321b84cb4b41b7e9eec
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-04-07 17:25:15 +0200

    Added exercises

---
+++ README.md
@@ -0,0 +1,12 @@
+# Opdracht beschrijving
+
+Deze opdracht bestaat uit meerdere opdrachtjes om geïsoleerd met destructuring te kunnen oefenen. Om de opdracht te maken kun je de opdracht clonen of downloaden naar jouw eigen computer.
+
+Dit project bevat de volgende bestanden:
+
+1. `destructuring.js`
+
+## Script runnen
+Als je de code wil runnen kun je dit doen door het volgende in de terminal in te voeren:
+
+`nodemon destructuring.js`
---
+++ destructuring.js
@@ -0,0 +1,64 @@
+// Opdracht 1: Destructure price en refreshRate uit onderstaand object, en log ze daarna in de terminal
+
+const product = {
+  price: 379,
+  refreshRate: 50,
+  screenType: 'LED',
+}
+
+// Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
+
+const productInformation = {
+  general: {
+    type: '43PUS6504/12',
+    name: '4K TV',
+    brand: 'Philips',
+  },
+  screenQuality: 'Ultra HD/4K',
+  smartTv: true,
+}
+
+// Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
+
+const tvOptions = {
+  options: {
+    colors: [
+      'black',
+      'silver',
+      'gold',
+      'sand',
+    ],
+    connectivity: {
+      popular: {
+        wifi: true,
+        bluetooth: false,
+      },
+      infrared: false,
+    }
+  },
+};
+
+// Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
+
+const connectivity = ['wifi', 'bluetooth'];
+
+
+// Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
+
+const tvSizes = [41, 43, 46, 56, 65];
+
+
+// opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
+
+function getCompanyDetails() {
+  return {
+    name: 'Novi Hogeschool',
+    adress: 'Zonnebaan 9, Utrecht',
+  }
+}
+
+// opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
+
+function getDetails(name, age) {
+  return [`Your name is ${name}`, `You are ${age} years old`];
+}
